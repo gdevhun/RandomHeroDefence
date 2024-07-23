910edaa75df05a1c0fced849cd63468961a60fcc
gdevhun <wjh9330@naver.com>
2024-07-23 14:26:42 +0900

    Feat : Enemy Prefab Anim Transition

--- Assets/02.Script/Character/EnemyBase.cs
+++ Assets/02.Script/Character/EnemyBase.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBase : MonoBehaviour
 {
-    [SerializeField] private int maxHp;
+    private Animator animator;
+    [SerializeField] private int maxHp; //초기설정 Hp
     private int currentHp;
-    [SerializeField] private int moveSpeed;
-    
+    [SerializeField] private int moveSpeed; //몬스터이동속도
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Start()
     {
         currentHp = maxHp;
@@ -26,8 +33,13 @@ public class EnemyBase : MonoBehaviour
 
     private void Die()
     {
-        //비활성화후
+        animator.SetTrigger("isDead");
         //풀에 다시 집어넣기
+    }
+
+    public void InactiveObj()
+    {
+        gameObject.SetActive(false); //에니메이션 데드이벤트 끝쪽에서 호출
     }
     void Update()
     {
