acd7a8e6bbf488999e16f51a7a7923655b4667ec
LeeJungHwi <101587101+LeeJungHwi@users.noreply.github.com>
2024-08-19 18:17:21 +0900

    Fix : Loading Bar

--- Assets/02.Script/Managers/SceneCtrlManager/DynamicBtn.cs
+++ Assets/02.Script/Managers/SceneCtrlManager/DynamicBtn.cs
@@ -7,11 +7,11 @@ public class DynamicBtn : MonoBehaviour
 {
     private void Start()
     {
-        /*SceneCtrlManager.instance.startBtn = GameObject.FindGameObjectWithTag("StartBtn").GetComponent<Button>();
+        SceneCtrlManager.instance.startBtn = GameObject.FindGameObjectWithTag("StartBtn").GetComponent<Button>();
         SceneCtrlManager.instance.exitBtn = GameObject.FindGameObjectWithTag("ExitBtn").GetComponent<Button>();
         SceneCtrlManager.instance.loadingBar = GameObject.FindGameObjectWithTag("LoadingBar");
         SceneCtrlManager.instance.startBtn.onClick.AddListener(() => SceneCtrlManager.instance.AsyncLoadScene("GameScene"));
         SceneCtrlManager.instance.exitBtn.onClick.AddListener(SceneCtrlManager.instance.ExitGame);
-        SoundManager.instance.BgmSoundPlay(BgmType.게임메뉴);*/
+        SoundManager.instance.BgmSoundPlay(BgmType.게임메뉴);
     }
 }
--- Assets/02.Script/Managers/SceneCtrlManager/SceneCtrlManager.cs
+++ Assets/02.Script/Managers/SceneCtrlManager/SceneCtrlManager.cs
@@ -9,12 +9,9 @@ public class SceneCtrlManager : MonoBehaviour
 {
     // 싱글톤
     public static SceneCtrlManager instance;
-    
-    [HideInInspector] public GameObject introEffect;
-    [HideInInspector] public GameObject loadingCircle;
+    [HideInInspector] public GameObject loadingBar;
     [HideInInspector] public Button startBtn;
     [HideInInspector] public Button exitBtn;
-    
     private readonly WaitForSeconds delay = new WaitForSeconds(2.5f);
     
     private void Awake()
@@ -25,64 +22,11 @@ public class SceneCtrlManager : MonoBehaviour
 
             // 씬 전환 시 파괴 X
             DontDestroyOnLoad(instance);
-
         }
         else Destroy(gameObject);
     }
 
-    private void OnEnable() => SceneManager.sceneLoaded += OnSceneLoaded;
-    private void OnDisable() => SceneManager.sceneLoaded -= OnSceneLoaded;
-    
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) 
-    {   //씬이 로드되면 호출 되는 (이벤트 리스너) 함수
-        
-        // 메뉴씬이 완전히 로드된 후 호출될 코드
-        if (scene.name == "MenuScene")
-        {
-            OnMenuSceneLoaded();
-        }
-        // 게임씬이 완전히 로드된 후 호출될 코드
-        else if (scene.name == "GameScene") 
-        {
-            OnGameSceneLoaded();
-        }
-    }
-    private void CheckDontDestroyOnLoad(GameObject obj)
-    {
-        if (obj.scene.name != "DontDestroyOnLoad")
-        {
-            DontDestroyOnLoad(loadingCircle); 
-            DontDestroyOnLoad(introEffect);
-        }
-    }
-    private void OnMenuSceneLoaded()
-    {
-        SoundManager.instance.BgmSoundPlay(BgmType.게임메뉴);
-        
-        loadingCircle = GameObject.FindGameObjectWithTag("LoadingCircle");
-        introEffect = GameObject.FindGameObjectWithTag("IntroEffect");
-        loadingCircle.SetActive(true);
-        introEffect.SetActive(true);
-        
-        CheckDontDestroyOnLoad(introEffect);
-        CheckDontDestroyOnLoad(loadingCircle);
-        
-        startBtn = GameObject.FindGameObjectWithTag("StartBtn").GetComponent<Button>();
-        exitBtn = GameObject.FindGameObjectWithTag("ExitBtn").GetComponent<Button>();
-        startBtn.onClick.AddListener(() => AsyncLoadScene("GameScene"));
-        exitBtn.onClick.AddListener(ExitGame);
-    }
-
-
-    private void OnGameSceneLoaded()
-    {
-        loadingCircle.SetActive(false);
-        introEffect.SetActive(false);
-        
-    }
-
-    private void SetLoadingBar() => loadingCircle.transform.localPosition=(new Vector3(815, 462f, 0));
-    private void ResetLoadingBar() => loadingCircle.transform.localPosition=(new Vector3(1064,462,0));
+    private void SetLoadingBar() => loadingBar.transform.localPosition=(new Vector3(815, 462f, 0));
     
     public void AsyncLoadScene(string nextScene)
     {
@@ -102,7 +46,6 @@ public class SceneCtrlManager : MonoBehaviour
                 if (asyncOperation.progress >= 0.9f)
                 {
                     yield return delay;
-                    ResetLoadingBar();
                     asyncOperation.allowSceneActivation = true;
                 }
                 yield return null;
@@ -118,5 +61,4 @@ public class SceneCtrlManager : MonoBehaviour
     {
         Application.Quit();
     }
-    
 }
