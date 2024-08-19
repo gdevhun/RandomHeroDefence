ad4ee4941466ae911430d3b3f80f85fa21292e6a
LeeJungHwi <101587101+LeeJungHwi@users.noreply.github.com>
2024-08-19 23:26:28 +0900

    Fix : Max stage boss

--- Assets/02.Script/Managers/GameManager.cs
+++ Assets/02.Script/Managers/GameManager.cs
@@ -50,14 +50,14 @@ public class GameManager : MonoBehaviour
         {
             GameResume();
         }
-        
     }
 
     // 10초 뒤 게임 시작
     public IEnumerator GameStartRoutine()
     {
         for(int i = 0; i < 10; i++) yield return StageManager.instance.oneSecond;
-        StageManager.instance.StartStage(StageManager.instance.CurStage);
+        StageManager.instance.CurStage = 40;
+        //StageManager.instance.StartStage(StageManager.instance.CurStage);
     }
 
     private IEnumerator ShowGameResultPanel(PlayerResType playerResType)
